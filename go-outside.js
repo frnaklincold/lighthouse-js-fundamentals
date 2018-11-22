1c656a994ad295ca9b28c6f429357c95c09819f6
franklincold <franklincold1012@gmail.com>
2018-11-22 05:00:37 +0000

    Adds looping exercises

--- go-outside.js
+++ go-outside.js
@@ -1,10 +1,19 @@
-var raining =false
-;
-var cold = false;
-
-
-if (!raining) {
-  console.log("Leave your umbrella at home!");
+function whichSchool(age) {
+  if (age<13 ){
+    console.log('Elementary School');
+  }
+  if (age >= 13 && age <= 18){
+    console.log('Secondary School');
+  }
+  else congsole.log('Lighthouse Labs');
+  // Your code in here ...
 }
 
-console.log("Now you're ready to go outside!");
+
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
